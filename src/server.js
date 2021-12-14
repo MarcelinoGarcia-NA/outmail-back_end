e950cca185710061fd763953bca74c8d6a8a308e
MarcelinoGarcia-NA <64509713+MarcelinoGarcia-NA@users.noreply.github.com>
2021-12-14 17:17:45 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require("./routes");
 const cors= require("cors");
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3000;
 mongoose.connect("mongodb://localhost:27017/project_email",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
